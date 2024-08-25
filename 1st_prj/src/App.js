f1ebd84c06ca821c54f3f2380bfe479166b0af8d
김규성 <hadugo@gmail.com>
2024-08-25 17:11:32 +0900

    1st

---
+++ 1st_prj/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hello, { Welcome } from './component/HellowComponent';
+function App() {
+
+  return (
+
+    <div className="App">
+      <Hello/>
+      <Welcome/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ 1st_prj/src/component/HellowComponent.js
@@ -0,0 +1,14 @@
+const Hello = () => {
+    return (
+        <p>Hellow</p>
+    )
+}
+
+export default Hello
+
+export function Welcome(){
+    return (
+        <p>Welcome</p>
+    )
+
+}
