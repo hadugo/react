19fcab7e296da07930de766bec9d022948f87ddf
김규성 <hadugo@gmail.com>
2024-08-26 12:47:54 +0900

    event

--- 1st_prj/src/component/HellowComponent.js
+++
@@ -1,8 +0,0 @@
-import hellowStyle from'./HellowComponent.module.css'
-const Hello = () => {
-    return (
-        <div className={hellowStyle.hellow}>Hellow</div>
-    )
-}
-
-export default Hello
