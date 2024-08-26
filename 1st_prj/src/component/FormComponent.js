80e82c2b6ece1bafaf912058bcaae4b298628e6a
김규성 <hadugo@gmail.com>
2024-08-26 13:56:53 +0900

    준비

--- 1st_prj/src/component/FormComponent.js
+++ 1st_prj/src/component/FormComponent.js
@@ -2,17 +2,9 @@ import { useState } from "react"
 
 export default function(){
 
-    const [ greetings, setGreetings ] = useState("hellow")
-
-    const changeGreetings = function(){
-        const value = (greetings === 'hellow') ? 'hi' : 'hellow'
-        setGreetings(value)
-    }
-    
     return (
         <div>
-            <button onClick={ changeGreetings }>Change</button>
-            <p>{ greetings }</p>
+            <p>{  }</p>
         </div>
     )
 
